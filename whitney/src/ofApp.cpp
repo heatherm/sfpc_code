353e2ed8e82dcb589a3e9e36ff04c6f41d3a8968
heathermoore <moore.hj@gmail.com>
2017-10-03 06:31:04 -0400

    add glxinfo project & update whitney to blackhole with red/yellow suns

--- whitney/src/ofApp.cpp
+++ whitney/src/ofApp.cpp
@@ -1,27 +1,20 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
     gui.setup();
-    gui.add(scaleFactor.setup("scaleFactor", 100, 400, 10));
+    gui.add(scaleFactor.setup("scaleFactor", 0.1, 10, .3));
     
     soundPlayer.load("sounds/taro.mp3");
     soundPlayer.play();
-        
-    ofSetVerticalSync(true);
-    ofBackground(0);
     
     int bufferSize = 256;
-    
     left.assign(bufferSize, 0.0);
     right.assign(bufferSize, 0.0);
-    
-    bufferCounter	= 0;
-    drawCounter		= 0;
     smoothedVol     = 0.0;
     scaledVol		= 0.0;
     
-        
+    
     ofSoundStreamSetup(0, 2, this, 44100, bufferSize, 2);
     
     auto devices = soundStream.getMatchingDevices("default");
@@ -29,50 +22,88 @@ void ofApp::setup(){
         soundStream.setDevice(devices[0]);
     }
     
+    cameraRotation.set(0);
+    zoom = -500;
+    zoomTarget = 200;
+    
+    billboards.getVertices().resize(NUM_BILLBOARDS);
+    billboards.getColors().resize(NUM_BILLBOARDS);
+    billboards.getNormals().resize(NUM_BILLBOARDS,ofVec3f(0));
+    
+    for (int i=0; i<NUM_BILLBOARDS; i++) {
+        billboardVels[i].set(ofRandomf(), -1.0, ofRandomf());
+        billboards.getVertices()[i] = {ofRandom(-500, 500),ofRandom(-500, 500),ofRandom(-500, 500)};
+        
+        billboards.getColors()[i].set(ofColor::fromHsb(ofRandom(0, 45), 255, 255));
+        billboardSizeTarget[i] = ofRandom(15, 100);
+    }
+    
+    billboards.setUsage(GL_DYNAMIC_DRAW);
+    billboards.setMode(OF_PRIMITIVE_POINTS);
+    billboardShader.load("shadersGL2/Billboard");
+
+    ofDisableArbTex();
+    texture.load("spark.png");
+    ofEnableAlphaBlending();
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
     ofScopedLock waveformLock(mutex);
     
     scaledVol = ofMap(smoothedVol, 0.0, 0.17, 0.0, 1.0, true);
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
     
-    ofSetColor(225);
-    gui.draw();
+    float t = (ofGetElapsedTimef()) * 0.9f;
+    float div = 250.0;
     
-    ofNoFill();
-
-    ofPushStyle();
-    ofPushMatrix();
-    ofTranslate(0, 0, 0);
-    
-    ofSetColor(225);
-    ofDrawBitmapString("Scaled average vol (0-100): " + ofToString(scaledVol * 100.0, 0), 4, 18);
-    
-    ofFill();
-    
-    float time = ofGetElapsedTimef();
-    ofSetCircleResolution(100);
-    for (size_t i = 0 ; i < ofGetWidth(); i++){
-        ofSetColor(
-                   102,
-                   178+77*cos(ofGetElapsedTimef()+ i*0.013),
-                   178+77*cos(ofGetElapsedTimef()+ (i*0.014))
-                   );
-
-        ofDrawCircle(i, ofGetHeight()/2 + (scaledVol * scaleFactor) *sin(i*.03+time*1.4), 60+40*sin(i*0.01 + time));
+    for (int i=0; i<NUM_BILLBOARDS; i++) {
+        ofVec3f vec(ofSignedNoise(t, billboards.getVertex(i).y/div, billboards.getVertex(i).z/div),
+                    ofSignedNoise(billboards.getVertex(i).x/div, t, billboards.getVertex(i).z/div),
+                    ofSignedNoise(billboards.getVertex(i).x/div, billboards.getVertex(i).y/div, t));
+        
+        vec *= 10 * ofGetLastFrameTime();
+        billboardVels[i] += vec;
+        billboards.getVertices()[i] += billboardVels[i];
+        billboardVels[i] *= 0.94f;
+        billboards.setNormal(i,ofVec3f(12 + billboardSizeTarget[i] * ofNoise(t+i),0,0));
     }
     
     
-    ofPopMatrix();
-    ofPopStyle();
+    float mx = (float)scaledVol*scaleFactor/(float)ofGetWidth();
+    float my = (float)scaledVol*scaleFactor/(float)ofGetHeight();
+    ofVec3f des(mx * 360.0, my * 360.0, 0);
+    cameraRotation += scaledVol*20;
 
-    drawCounter++;
+    zoom += (zoomTarget - zoom) * scaleFactor*scaledVol;
     
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+    ofBackgroundGradient(0, 60);
+    
+    gui.draw();
+
+    ofSetColor(255);
+    
+    ofPushMatrix();
+    ofTranslate(ofGetWidth()/2, ofGetHeight()/2, zoom);
+    ofRotate(cameraRotation.x, 1, 0, 0);
+    ofRotate(cameraRotation.y, 0, 1, 0);
+    ofRotate(cameraRotation.y, 0, 0, 1);
+    
+
+    billboardShader.begin();
+    
+    ofEnablePointSprites(); // for GL2
+    texture.getTexture().bind();
+    billboards.draw();
+    texture.getTexture().unbind();
+    ofDisablePointSprites(); // for GL2
+    
+    billboardShader.end();
+    
+    ofPopMatrix();
 }
 
 //--------------------------------------------------------------
@@ -98,13 +129,15 @@ void ofApp::audioIn(ofSoundBuffer & input){
     
     smoothedVol *= 0.93;
     smoothedVol += 0.07 * curVol;
-    
-    bufferCounter++;
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed  (int key){
+void ofApp::keyPressed(int key){
+    if(key == 'f') ofToggleFullscreen();
+    if(key == OF_KEY_UP) zoomTarget +=10;
+    if(key == OF_KEY_DOWN) zoomTarget -=10;
 }
+
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
@@ -112,7 +145,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y){
     
 }
 
@@ -152,6 +185,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
     
 }
--- whitney/src/ofApp.h
+++ whitney/src/ofApp.h
@@ -1,19 +1,19 @@
 #pragma once
-
 #include "ofMain.h"
+#include "ofVbo.h"
 #include "ofxGui.h"
 
-class ofApp : public ofBaseApp{
-    
+#define NUM_BILLBOARDS 600
+
+class ofApp : public ofBaseApp {
 public:
-    
     void setup();
     void update();
     void draw();
     
     void keyPressed(int key);
     void keyReleased(int key);
-    void mouseMoved(int x, int y );
+    void mouseMoved(int x, int y);
     void mouseDragged(int x, int y, int button);
     void mousePressed(int x, int y, int button);
     void mouseReleased(int x, int y, int button);
@@ -22,24 +22,30 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
-				
+    
+    // billboard
+    ofVec3f cameraRotation;
+    float zoom, zoomTarget;
+    float billboardSizeTarget[NUM_BILLBOARDS];
+    ofShader billboardShader;
+    ofImage texture;
+    ofVboMesh billboards;
+    ofVec3f billboardVels[NUM_BILLBOARDS];
+    
+    // audio
     void audioIn(ofSoundBuffer & input);
     
     vector <float> left;
     vector <float> right;
-    vector <float> volHistory;
-    
-    int 	bufferCounter;
-    int 	drawCounter;
-    
+    vector <float> volHistory
+    int bufferCounter;
     float smoothedVol;
     float scaledVol;
-    
     ofSoundStream soundStream;
     ofSoundPlayer soundPlayer;
-    
     ofMutex mutex;
     
+    // gui
     ofxPanel gui;
     ofxFloatSlider scaleFactor;
 };
